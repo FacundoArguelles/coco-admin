8818ac1b0157b844db33f2861a325fb56aa7abd7
Facundo Argüelles <ffarguelles@gmail.com>
2018-09-25 20:15:55 -0300

    empeze con el date

--- coco_admin.py
+++ coco_admin.py
@@ -3,6 +3,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import *
+from datetime import date
 import sys
 import csv
 
@@ -30,7 +31,11 @@ class Window(QWidget):
 		#BUTTON
 		button = QPushButton("CREAR")
 		button.clicked.connect(self.create)
-		layout.addWidget(button,1,7)
+		layout.addWidget(button,1,8)
+
+		but_today = QPushButton('¿HOY?')
+		#but_today.clicked.connect(self.set_date_today)
+		layout.addWidget(but_today,1,4)
 
 		but_stats = QPushButton("Estadisticas")
 		#but_stats.clicked.connect(self.show_stats)
@@ -57,41 +62,46 @@ class Window(QWidget):
 		self.dia = QComboBox()
 		for num in range(31):
 			self.dia.addItem(str(num+1))
-		layout.addWidget(self.dia,1,4)
+		layout.addWidget(self.dia,1,5)
 
 		self.mes1 = QComboBox()
 		meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 		for mes in meses:
 			self.mes1.addItem(mes)
-		layout.addWidget(self.mes1,1,5)
+		layout.addWidget(self.mes1,1,6)
 
 		self.anio = QComboBox()
-		for num in range(1980,2020):
+		for num in range(2017,2020):
 			self.anio.addItem(str(num))
-		layout.addWidget(self.anio,1,6)
+		layout.addWidget(self.anio,1,7)
 
 		#TABLE
 		self.tabla = QTableWidget()
 		self.tabla.setColumnCount(5)
-		self.tabla.setRowCount(1)
+		self.tabla.setRowCount(0)
 		self.tabla.setShowGrid(True)
 		self.tabla.setHorizontalHeaderLabels(('Marca','Plataforma','Cantidad','Valor','Fecha'))
-		layout.addWidget(self.tabla,2,0,1,8)
+		layout.addWidget(self.tabla,2,0,1,9)
 		with open('datos.csv', mode='r') as csv_file:
 			datareader = csv.reader(csv_file)
 			for index, row in enumerate(datareader):
 				rr = self.tabla.rowCount() + 1
 				self.tabla.setRowCount(rr)
 				for index2, el in enumerate(row):
-					#print(index,index2)
 					self.tabla.setItem(index,index2,QTableWidgetItem(el))
-				
+		
+		
+	#FUNCION SET_DATE_TODAY
+	def set_date_today(self):
+		
+
+
 
 	#FUNCION ESTADISTICAS
 	# def show_stats(self):
 	# 	with open('datos.csv', mode='r') as csv_file:
 	# 		datareader = csv.reader(csv_file)
-	# 		for row in datareader:
+	# 		for index, row in enumerate(datareader):
 	# 			for el in row:
 	
 
@@ -116,7 +126,7 @@ class Window(QWidget):
 		m = self.mes1.currentText()
 		a = self.anio.currentText()
 
-		lista = [ma,plat,cant,val,d,m,a]
+		lista = [ma,plat,cant,val,[d,m,a]]
 
 		with open('datos.csv', mode='a') as csv_file:
 			datawriter = csv.writer(csv_file, delimiter=",")
